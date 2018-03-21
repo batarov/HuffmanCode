7afa2eeef057730b66fa07539e2e21aa5c81d5d1
Dmitry Batarov <nonetype@gmail.com>
2018-03-21 23:31:54 +0300

    Improved output

--- encoder/main.cpp
+++ encoder/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <cassert>
 
 #include <huffmanencoder.h>
 
 using namespace std;
+
+static size_t GetFileSize(const char* aFilename)
+{
+    File file(aFilename);
+    return file.Open(File::ReadOnlyBinary) ? file.Size() : 0;
+}
 
 int main(int argc, char** argv)
 {
@@ -17,6 +22,13 @@ int main(int argc, char** argv)
     if (!encoder.Encode(argv[1], argv[2]))
     {
         cout << "File could not be encoded" << endl;
+    }
+    else
+    {
+        auto size = GetFileSize(argv[1]);
+        auto packedSize = GetFileSize(argv[2]);
+
+        cout << "Size " << size << " Packed size " << packedSize << " Ratio " << ((double)packedSize / size) << endl;
     }
 
     return 0;
